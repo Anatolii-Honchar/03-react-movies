07ab8404acd381e79a2488595e472ba2a88f7d4f
Anatolii Honchar <gonchar_tol@ukr.net>
2026-03-05 22:32:03 +0200

    feat: add axios for API requests and implement movie fetching functionality

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,17 +1,50 @@
-import css from "./App.module.css";
-import SearchBar from "../SearchBar/SearchBar";
 import { useState } from "react";
+import toast from "react-hot-toast";
+
+import styles from "./App.module.css";
+
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
 
 export default function App() {
-  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSearchSubmit = (searchQuery: string) => {
-    setQuery(searchQuery);
+  const handleSearch = async (query: string) => {
+    try {
+      setIsLoading(true);
+
+      // очищаємо попередній результат
+      setMovies([]);
+
+      const data = await fetchMovies(query);
+
+      if (data.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+
+      setMovies(data);
+    } catch {
+      toast.error("Something went wrong. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
-    <div className={css.app}>
-      <SearchBar onSubmit={handleSearchSubmit} />
+    <div className={styles.app}>
+      <SearchBar onSubmit={handleSearch} />
+
+      {isLoading && <Loader />}
+
+      {!isLoading && movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={() => {}} />
+      )}
     </div>
   );
 }
